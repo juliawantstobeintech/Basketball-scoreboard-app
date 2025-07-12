c4edce0d2c8a8ffa025aaf1046f3ad7ba1eec9d7
juliawantstobeintech <103024211+juliawantstobeintech@users.noreply.github.com>
2025-07-12 17:16:11 -0600

    smoothed JS and CSS code
    added onclick events on JS to smooth deployment.
    Edited CSS code for case sensitivity and removed incorrect properties

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 let homeScore = document.getElementById("home-score")
 let guestScore = document.getElementById("guest-score")
+// adding event listeners to the buttons to update or reset the scores
+document.getElementById("addOneHome").addEventListener("click", oneHome)
+document.getElementById("addTwoHome").addEventListener("click", twoHome)
+document.getElementById("addThreeHome").addEventListener("click", threeHome)
+document.getElementById("addOneGuest").addEventListener("click", oneGuest)
+document.getElementById("addTwoGuest").addEventListener("click", twoGuest)
+document.getElementById("addThreeGuest").addEventListener("click", threeGuest)
+document.getElementById("newGame").addEventListener("click", reset)
+
+//initializing the scores to 0
 let homeStartScore = 0
 let guestStartScore = 0
 
-function reset(){
+function reset() {
     homeStartScore = 0
     homeScore.textContent = homeStartScore
     
